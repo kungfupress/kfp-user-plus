5411ffbe0fd4c3d46695592beb3bdaf72680c878
Juanan Ruiz <juanan@us.es>
2019-09-12 20:19:24 +0200

    Graba correctamente redes sociales y datos de dirección

--- kfp-user-plus.php
+++ kfp-user-plus.php
@@ -7,45 +7,70 @@
  * Author URI:   https://kungfupress.com/
  */
 
-add_action( 'show_user_profile', 'user_plus_profile_fields' );
-add_action( 'edit_user_profile', 'user_plus_profile_fields' );
+// Para agregar información de contacto de redes sociales y demás
+function modify_contact_methods($profile_fields) {
 
-function user_plus_profile_fields( $user ) { ?>
-    <h3><?php _e("Pérfil plus de usuario", "blank"); ?></h3>
+	$profile_fields['twitter'] = 'Twitter';
+	$profile_fields['facebook'] = 'Facebook (URL)';
+
+	return $profile_fields;
+}
+add_filter('user_contactmethods', 'modify_contact_methods');
+
+// Para cualquier otro tipo de información
+add_action( 'show_user_profile', 'user_plus_profile_fields', 10, 1 );
+add_action( 'edit_user_profile', 'user_plus_profile_fields', 10, 1 );
+add_action( 'personal_options_update', 'save_user_plus_profile_fields' );
+add_action( 'edit_user_profile_update', 'save_user_plus_profile_fields' );
+
+function user_plus_profile_fields( $user ) { 
+    ob_start();
+    ?>
+    <h3><?php _e("Domicilio", "blank"); ?></h3>
 
     <table class="form-table">
     <tr>
-        <th><label for="address"><?php _e("Address"); ?></label></th>
+        <th><label for="address"><?php _e("Dirección"); ?></label></th>
         <td>
-            <input type="text" name="address" id="address" value="<?php echo esc_attr( get_the_author_meta( 'address', $user->ID ) ); ?>" class="regular-text" /><br />
-            <span class="description"><?php _e("Please enter your address."); ?></span>
+            <input type="text" name="address" id="address" class="regular-text"
+                value="<?php echo esc_attr( get_the_author_meta( 'address', $user->ID ) ); ?>"><br />
+            <span class="description"><?php _e("Por favor, indica tu dirección."); ?></span>
         </td>
     </tr>
     <tr>
-        <th><label for="city"><?php _e("City"); ?></label></th>
+        <th><label for="city"><?php _e("Ciudad"); ?></label></th>
         <td>
-            <input type="text" name="city" id="city" value="<?php echo esc_attr( get_the_author_meta( 'city', $user->ID ) ); ?>" class="regular-text" /><br />
-            <span class="description"><?php _e("Please enter your city."); ?></span>
+            <input type="text" name="city" id="city" class="regular-text"
+                value="<?php echo esc_attr( get_the_author_meta( 'city', $user->ID ) ); ?>"><br />
+            <span class="description"><?php _e("Por favor, indica tu ciudad."); ?></span>
         </td>
     </tr>
     <tr>
-    <th><label for="postalcode"><?php _e("Postal Code"); ?></label></th>
+    <th><label for="postalcode"><?php _e("Código postal"); ?></label></th>
         <td>
-            <input type="text" name="postalcode" id="postalcode" value="<?php echo esc_attr( get_the_author_meta( 'postalcode', $user->ID ) ); ?>" class="regular-text" /><br />
-            <span class="description"><?php _e("Please enter your postal code."); ?></span>
+            <input type="text" name="postalcode" id="postalcode"  class="regular-text"
+                value="<?php echo esc_attr( get_the_author_meta( 'postalcode', $user->ID ) ); ?>"><br />
+            <span class="description"><?php _e("Por favor, indica tu código postal."); ?></span>
+        </td>
+    </tr>
+    <th><label for="province"><?php _e("Provincia"); ?></label></th>
+        <td>
+            <input type="text" name="province" id="province"  class="regular-text"
+                value="<?php echo esc_attr( get_the_author_meta( 'province', $user->ID ) ); ?>"><br />
+            <span class="description"><?php _e("Por favor, indica tu provincia."); ?></span>
         </td>
     </tr>
     </table>
-<?php }
-
-add_action( 'personal_options_update', 'save_user_plus_profile_fields' );
-add_action( 'edit_user_profile_update', 'save_user_plus_profile_fields' );
+    <?php
+    echo ob_get_clean();
+}
 
 function save_user_plus_profile_fields( $user_id ) {
     if ( !current_user_can( 'edit_user', $user_id ) ) { 
         return false; 
     }
-    update_user_meta( $user_id, 'address', $_POST['address'] );
-    update_user_meta( $user_id, 'city', $_POST['city'] );
-    update_user_meta( $user_id, 'postalcode', $_POST['postalcode'] );
+    update_user_meta( $user_id, 'address', sanitize_text_field($_POST['address'] ));
+    update_user_meta( $user_id, 'city', sanitize_text_field($_POST['city'] ));
+    update_user_meta( $user_id, 'postalcode', sanitize_text_field($_POST['postalcode'] ));
+    update_user_meta( $user_id, 'province', sanitize_text_field($_POST['province'] ));
 }
